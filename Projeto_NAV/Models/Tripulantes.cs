f57bf3a4b02a5a8e0afaafb82d90ccaa1317ba5f
matheushermsdorff808-lang <00001085383398sp@aluno.educacao.sp.gov.br>
2026-07-28 16:02:03 -0300

    Add classe tripulantes

---
+++ Projeto_NAV/Models/Tripulantes.cs
@@ -0,0 +1,6 @@
+﻿namespace Projeto_NAV.Models
+{
+    public class Tripulantes
+    {
+    }
+}
--- Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
+++ Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b228a4013e2db5d3e3d65dbbd80ae1b6ca0c46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
