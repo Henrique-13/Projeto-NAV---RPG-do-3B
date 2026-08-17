cee3faac6825eabbac8b2f501734bc2826094765
matheushermsdorff808 <matheushermsdorff808@gmail.com>
2026-08-17 16:19:06 -0300

    correção de conflitos

--- Projeto_NAV/Controllers/PerfilController.cs
+++ Projeto_NAV/Controllers/PerfilController.cs
@@ -8,45 +8,43 @@ namespace Projeto_NAV.Controllers
         {
             return View();
         }
-<<<<<<< HEAD
+
         public IActionResult pepe()
-=======
-        public IActionResult Marina()
->>>>>>> 1ca8739d27efeb3b2d21b6f0b2d2d653d46a0bbf
-        {
-            return View();
-        }
-        public IActionResult Teste()
-<<<<<<< HEAD
-=======
         {
             return View();
         }
 
-<<<<<<< HEAD
+        public IActionResult Marina()
+
+        {
+            return View();
+        }
+        public IActionResult Teste()
+
+        {
+            return View();
+        }
+
+
         public IActionResult bigrauu_()
         {
             return View();
         }
 
-=======
         public IActionResult Thiago()
->>>>>>> 1ca8739d27efeb3b2d21b6f0b2d2d653d46a0bbf
+
         {
             return View();
         }
-<<<<<<< HEAD
         public IActionResult matheus() 
         {
             return View();
         }
-=======
         public IActionResult gustavosilveira()
         {
             return View();
         }
         
->>>>>>> 2c9cd53d2b2ec9ff2ea842c848d72cb711f47429
->>>>>>> b2a74fda1da4085d573a78a0cbb77d6cbfc94eb0
+
     }
 }
