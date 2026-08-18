d2da32147504bcc60c6d53b4f215fbfc012e66a4
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-08-18 11:15:56 -0300

    ajustando erro site tião

--- Projeto_NAV/Controllers/PerfilController.cs
+++ Projeto_NAV/Controllers/PerfilController.cs
@@ -9,43 +9,33 @@ namespace Projeto_NAV.Controllers
             return View();
         }
 
-<<<<<<< HEAD
+
         public IActionResult pepe()
-=======
+        {
+            return View();
+        }
+
         public IActionResult Teste()
         {
             return View();
         }
         public IActionResult Nilson()
->>>>>>> a2b63e4115018fd58548368946daafae66be4c3c
         {
             return View();
         }
-
-<<<<<<< HEAD
-=======
-        public IActionResult pepe()
-        {
-            return View();
-        }
-
->>>>>>> a2b63e4115018fd58548368946daafae66be4c3c
         public IActionResult Marina()
 
         {
             return View();
         }
-<<<<<<< HEAD
-        public IActionResult Teste()
 
-=======
+    
       
         public IActionResult Sarah()
->>>>>>> a2b63e4115018fd58548368946daafae66be4c3c
+
         {
             return View();
         }
-
 
         public IActionResult bigrauu_()
         {
@@ -53,34 +43,24 @@ namespace Projeto_NAV.Controllers
         }
 
         public IActionResult Thiago()
-<<<<<<< HEAD
+
 
         {
             return View();
         }
-=======
-        {
-            return View();
-        }
 
->>>>>>> a2b63e4115018fd58548368946daafae66be4c3c
+     
+
         public IActionResult matheus() 
         {
             return View();
         }
-<<<<<<< HEAD
+
         public IActionResult gustavosilveira()
         {
             return View();
         }
         
-=======
-
-        public IActionResult gustavosilveira()
-        {
-            return View();
-        }        
->>>>>>> a2b63e4115018fd58548368946daafae66be4c3c
 
     }
 }
--- Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
+++ Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a9452663af6901cf7b813e94d287a676236a76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16059e9679c5b0b8a6b815b4190ae303a76a7c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
