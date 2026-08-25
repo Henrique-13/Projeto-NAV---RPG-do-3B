6f04e4f8c3915526c7a8c00e0c596ecec23adc58
GABRIEL PONTES GUEDES VERAS <00001137369917sp@aluno.educacao.sp.gov.br>
2026-08-25 15:11:08 -0300

    ajustes

--- Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
+++ Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfe79936cc54d926c542d8c77552985e935a3f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a729fbfcbb66642a46915690c510eb56f9bbe4ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
