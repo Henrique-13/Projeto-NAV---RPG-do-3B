0bac2a8a488bba491b8cc1f8de1a809862bd07a8
Lucas Felipe Ribeiro Queiroz <00001109958626sp@aluno.educacao.sp.gov.br>
2026-08-25 15:07:54 -0300

    perfil

--- Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
+++ Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2868caebad3f0805deffc3c0d6eab9e7d71e38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a6df42bf7e1e1bbfd82dc9fb0bdbc309c7d6f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
