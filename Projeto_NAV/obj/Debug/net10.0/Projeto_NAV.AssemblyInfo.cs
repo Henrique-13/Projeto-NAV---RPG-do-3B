964238bf50a4b13dd2173f8d3732858793125074
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-08-25 15:58:52 -0300

    Add Link Git na pagina de perfil

--- Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
+++ Projeto_NAV/obj/Debug/net10.0/Projeto_NAV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36422c6bc82f6d567a56f58c13e27c88d194df9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38bfbdd8bd48f3c828ca3992138fc953a2d51fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_NAV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
